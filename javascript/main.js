18a50f955fe0e9d2f072389d3e856132646dda69
Maino24 <mg02.gabriele@gmail.com>
2022-10-24 18:27:02 +0200

    inserite numero di partite vinte

--- javascript/main.js
+++ javascript/main.js
@@ -6,13 +6,13 @@
 //chiedo di inserire una mail
 
 
-/*let mail = prompt(`inseirsci la tua mail`);*/
+/*let mail = prompt(`inseirsci la tua mail`);
 
 
 // creo un ciclo in cui controllo che la mail inserita sia già esistente nell'array oppure no
 
 
-/*for (i = 0; i < mailPresenti.length; i++){
+for (i = 0; i < mailPresenti.length; i++){
 
     if (mail == mailPresenti[i]) {
         document.getElementById(`risposta`).innerHTML= `La mail inserita è già presente`
@@ -23,9 +23,9 @@
     
     
     console.log(mailPresenti[i])   
-}*/
+}
 
-
+*/
 
 
 
@@ -39,15 +39,30 @@
 
 let numeroGeneratoGiocatore = document.getElementById(`numeroGeneratoGiocatore`);
 let numeroGeneratoComputer = document.getElementById(`numeroGeneratoComputer`);
-let punteggio = document.getElementById(`punteggio`);
+let punteggioGiocatore = document.getElementById(`punteggioGiocatore`);
+let punteggioComputer = document.getElementById(`punteggioComputer`);
+
+punteggioComputer = 0 
+punteggioGiocatore = 0
 
 function genera(){
 
- document.getElementById(`numeroGeneratoGiocatore`).innerHTML=   Math.floor(Math.random() * 6) +1;
- document.getElementById(`numeroGeneratoComputer`).innerHTML=   Math.floor(Math.random() * 6) +1;
+ let numeroGeneratoGiocatore = document.getElementById(`numeroGeneratoGiocatore`).innerHTML=   Math.floor(Math.random() * 6) +1;
+ let numeroGeneratoComputer = document.getElementById(`numeroGeneratoComputer`).innerHTML=   Math.floor(Math.random() * 6) +1;
+
+
+
 
  if( numeroGeneratoComputer > numeroGeneratoGiocatore){
- 
+    punteggioComputer = punteggioComputer + 1
+    console.log(punteggioComputer)
+    document.getElementById(`punteggioComputer`).innerHTML= `Il Computer ha vinto : ${punteggioComputer} partite`
+}else if( numeroGeneratoGiocatore > numeroGeneratoComputer){
+    punteggioGiocatore = punteggioGiocatore + 1
+    console.log(punteggioGiocatore)
+    document.getElementById(`punteggioGiocatore`).innerHTML= `Il giocatore ha vinto : ${punteggioGiocatore} partite`
+}else/*(numeroGeneratoComputer === numeroGeneratoGiocatore)*/{
+    document.getElementById(`pareggio`).innerHTML= `Ritirare i dadi`
 }
 
 
